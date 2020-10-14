cba75e73bbd1c9d52bc4e529538e5fb42d11d69b
Patricia Matiesque <patymatiesque@gmail.com>
2020-10-14 15:44:09 -0300

    Ola mundo

---
+++ src/pacotePrimeiroPrograma/ClassePrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package pacotePrimeiroPrograma;
+
+public class ClassePrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Olá mundo");
+		System.out.print("Olá mundo");
+		System.out.print("Olá mundo");
+		System.out.print("Olá mundo");
+		System.out.print("Olá mundo");
+		System.out.print("Olá mundo");
+ 		
+	}
+
+}
---
+++ src/pacoteSegundoPrograma/Classe02.java
@@ -0,0 +1,10 @@
+package pacoteSegundoPrograma;
+
+public class Classe02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
